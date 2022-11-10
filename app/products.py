ee5eb182fddc0bf120b6799b2e39e2a079047cb4
Daniel Uranga <dsu2@duke.edu>
2022-11-10 00:44:52 -0500

    fixed none type error in avgReview, some name changes

--- app/products.py
+++ app/products.py
@@ -106,11 +106,16 @@ def productDetails(productid=None):
     sellers = Inventory.get_by_pid(id=productid)
     product = Product.get(id=productid)
     reviews = ProductReview.get_all_by_pid(productid)
-    avgreview = ProductReview.findAvgRating(pid =productid)
-    numreview = ProductReview.findNumReview(pid = productid)
+    avgreview = ProductReview.findAvgRating(pid=productid)
+    numreview = ProductReview.findNumReview(pid=productid)
 
     print(avgreview, file=sys.stdout)
     print(numreview, file=sys.stdout)
+
+    if avgreview==None:
+        avgreview = None
+    else:
+        avgreview = round(avgreview, 3)
 
 
     form = ReviewForm()
@@ -118,12 +123,12 @@ def productDetails(productid=None):
         if current_user.get_id() != None:
             ProductReview.addProductReview(pid = productid, uid = current_user.get_id(), text = form.text.data, rating = form.val.data)  
             reviews = ProductReview.get_all_by_pid(productid)
-    return render_template('product_detailed.html', avail_products = product, sellers=sellers, reviews = reviews, form = form, pid = productid, avgreview = avgreview, numreview =numreview)
+    return render_template('product_detailed.html', product= product, sellers=sellers, reviews = reviews, form = form, pid = productid, avgreview = avgreview, numreview =numreview)
 
  
 @bp.route('/pid/<int:productid>/editreview/', methods=['GET', 'POST'])
 def editReview(productid = None):
-    products = Product.get(id=productid)
+    product = Product.get(id=productid)
     form = ReviewForm()
 
     userid = current_user.get_id()
@@ -133,14 +138,14 @@ def editReview(productid = None):
         print('edit Form is Valid', file=sys.stdout)
         ProductReview.editProductReview(pid = productid, uid = userid, text = editform.text.data, rating = editform.val.data)
         reviews = ProductReview.get_all_by_pid(productid)
-        return render_template('product_detailed.html', avail_products = products, reviews = reviews, form = form, pid = productid)
+        return render_template('product_detailed.html', product= product, reviews = reviews, form = form, pid = productid)
 
    
     if removeform.validate_on_submit():
         print('Remove form is Valid', file=sys.stdout)
         ProductReview.removeProductReview(pid = productid, uid = userid)
         reviews = ProductReview.get_all_by_pid(productid)
-        return render_template('product_detailed.html', avail_products = products, reviews = reviews, form = form, pid = productid)
+        return render_template('product_detailed.html', product = product, reviews = reviews, form = form, pid = productid)
 
     return render_template('edit_review.html', editform = editform, removeform = removeform, productid = productid)
 
