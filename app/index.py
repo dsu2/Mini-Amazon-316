61f875a2ea8bd9dbe35a20fab7d8ffca1ba3babe
Jiayin Liu <108898526+jiayinliu25@users.noreply.github.com>
2022-11-11 14:48:49 -0500

    Update index.py

--- app/index.py
+++ app/index.py
@@ -1,4 +1,10 @@
-
+from flask import render_template, redirect, url_for, flash, request
+from werkzeug.urls import url_parse
+from flask_login import current_user
+import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, NumberRange
 from .models.product import Product
 from .models.purchase import Purchase
 from .models.reviews import ProductReview
