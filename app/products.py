386a690b3f0f0805d458c33c082d8284367482c9
Daniel Uranga <dsu2@duke.edu>
2022-12-01 20:57:46 -0500

    updated search, ability to add products

--- app/products.py
+++ app/products.py
@@ -5,7 +5,7 @@ import datetime
 import sys
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, SelectField, DecimalField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, NumberRange, AnyOf, Length, URL
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, NumberRange, AnyOf, Length, URL, Optional
 
 from .models.product import Product
 from .models.purchase import Purchase
@@ -18,16 +18,10 @@ from .models.seller import Seller
 from flask import Blueprint
 bp = Blueprint('products', __name__)
 
-class ExpensiveForm(FlaskForm):
-    k = IntegerField('Priciest number', validators=[DataRequired(), NumberRange(min=1, max =300)])
-    submit = SubmitField('sort')
-
-class CategoryForm(FlaskForm):
-    category = SelectField('Category', validators=[DataRequired()], choices=['Electronics', 'Decor', 'Grocery', 'Toys', 'Sports', 'Beauty', 'Automotive', 'Pets', 'Books', 'Movies', 'Games', 'Golf'])
-    submit = SubmitField('Go')
-
 class SearchForm(FlaskForm):
-    search = StringField('Search products by name', validators=[DataRequired(), Length(min=1, max=150)])
+    k = IntegerField('Priciest number', validators=[Optional(), NumberRange(min=1, max =300)])
+    category = SelectField('Category', validators=[Optional()], choices=['All Categories','Electronics', 'Decor', 'Grocery', 'Toys', 'Sports', 'Beauty', 'Automotive', 'Pets', 'Books', 'Movies', 'Games', 'Golf'])
+    search = StringField('Search products by name', validators=[Optional(), Length(min=1, max=150)])
     submit = SubmitField('Search')
 
 
@@ -58,90 +52,42 @@ class NewProductForm(FlaskForm):
 def shelf():
     
    
-   
-    form = ExpensiveForm()
-    products = Product.get_all(True)
+    form = SearchForm()
     if form.validate_on_submit():
-        top_x = form.k.data
-        products = Product.get_expensive_k(True,form.k.data)
+        products = Product.get_product_list(True, form.k.data, form.category.data, form.search.data)
     else:
-        top_x = None
-        
-
-    categoryForm = CategoryForm()
-    if categoryForm.validate_on_submit():
-        category = categoryForm.category.data
-        products = Product.get_category(True, categoryForm.category.data)
-    else:
-        category = None
-
-    searchForm = SearchForm()
-    if searchForm.validate_on_submit():
-        search_data = searchForm.search.data
-        products = Product.get_search(True, searchForm.search.data)
-    else: 
-        search_data = None
-
-    '''products = Product.get_product_list(top_x, category, search_data)'''
+        products = Product.get_all(True)
     
     return render_template('products.html',
                            avail_products=products,
-                           form=form, 
-                           categoryForm=categoryForm,
-                           searchForm=searchForm)
+                           form=form)
 
 @bp.route('/shelf/high2low', methods=['GET', 'POST'])
 def high2low():
     # get all available products for sale:
-    categoryForm = CategoryForm()
-    form = ExpensiveForm()
-    if form.validate_on_submit():
-        products = Product.get_expensive_k(True,form.k.data)
-    else:
-        products = Product.get_all(True)  
-    
     products = Product.get_most_expensive(True)
-    
-    categoryForm = CategoryForm()
-    if categoryForm.validate_on_submit():
-        products = Product.get_category(True, categoryForm.category.data)
 
-    searchForm = SearchForm()
-    if searchForm.validate_on_submit():
-        products = Product.get_search(True, searchForm.search.data)
+    form = SearchForm()
+    if form.validate_on_submit():
+        products = Product.get_product_list(True, form.k.data, form.category.data, form.search.data)
     
     return render_template('products.html',
                            avail_products=products,
-                           form=form, 
-                           categoryForm=categoryForm,
-                           searchForm=searchForm)
+                           form=form)
 
 @bp.route('/shelf/low2high', methods=['GET', 'POST'])
 def low2high():
     # get all available products for sale:
-    categoryForm = CategoryForm()
-    form = ExpensiveForm()
-    if form.validate_on_submit():
-        products = Product.get_expensive_k(True,form.k.data)
-    else:
-        products = Product.get_all(True)  
-    
     products = Product.get_least_expensive(True)
-    
-    categoryForm = CategoryForm()
-    if categoryForm.validate_on_submit():
-        products = Product.get_category(True, categoryForm.category.data)
 
-    searchForm = SearchForm()
-    if searchForm.validate_on_submit():
-        products = Product.get_search(True, searchForm.search.data)
+    form = SearchForm() 
+    if form.validate_on_submit():
+        products = Product.get_product_list(True, form.k.data, form.category.data, form.search.data)
 
     
     return render_template('products.html',
                            avail_products=products,
-                           form=form, 
-                           categoryForm=categoryForm,
-                           searchForm=searchForm)
+                           form=form)
 
 @bp.route('/pid/<int:productid>/', methods=['GET', 'POST'])
 def productDetails(productid=None):
